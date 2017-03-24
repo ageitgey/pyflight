dbf003e5c3aba340fc656e1f7fefff2c88347a40
Volcyy <Johannes.christ@gmail.com>
2017-03-24 09:37:22 +0100

    add setup.py, gitignore and init

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="pyflight",
+    version="0.0.1",
+    description="A Python Wrapper around Google's QPX Express API",
+    license="MIT",
+    author="Volcyy",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+    ]
+)
