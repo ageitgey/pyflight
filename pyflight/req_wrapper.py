5193e36e984b73a3c537acfb8ebb3deef8bf188e
Volcyy <Johannes.christ@gmail.com>
2017-05-31 15:33:55 +0200

    minor improvements of s

--- pyflight/req_wrapper.py
+++ pyflight/req_wrapper.py
@@ -1,8 +1,10 @@
 """
 Provides an easy-to-use interface to use pyflight with.
 """
-from pyflight.requests import requester
+from pyflight.requests import requester, Request
 from pyflight.results import Result
+
+from typing import Union
 
 BASE_URL = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key='
 api_key = ''
@@ -21,12 +23,13 @@ def set_api_key(key: str):
     api_key = key
 
 
-async def send_async(request_body: dict, use_containers: bool=True):
-    """Asynchronously execute and send a JSON Request. This is a coroutine - calling this function must be awaited.
+async def send_async(request_body: Union[dict, Request], use_containers: bool=True):
+    """Asynchronously execute and send a JSON Request or a :class:`Request`.
+     This is a coroutine - calling this function must be awaited.
     
     Parameters
     ----------
-    request_body : dict
+    request_body : Union[dict, Request]
         The body of the request to be sent to the API. This must follow the structure described here:
         https://developers.google.com/qpx-express/v1/trips/search
     use_containers : Optional[bool]
@@ -49,6 +52,8 @@ async def send_async(request_body: dict, use_containers: bool=True):
     """
     if isinstance(request_body, dict):
         response = await requester.post_request(BASE_URL + api_key, request_body)
+    elif isinstance(request_body, Request):
+        response = await requester.post_request(BASE_URL + api_key, request_body.raw_data)
     else:
         raise ValueError('Unsupported Request Type')
     if use_containers:
@@ -56,12 +61,12 @@ async def send_async(request_body: dict, use_containers: bool=True):
     return response
 
 
-def send_sync(request_body: dict, use_containers: bool=True):
-    """Synchronously execute and send a JSON-Request. Note that this function is blocking.
+def send_sync(request_body: Union[dict, Request], use_containers: bool=True):
+    """Synchronously execute and send a JSON-Request or a :class:`Request. Note that this function is blocking.
     
     Parameters
     ----------
-    request_body : dict
+    request_body : Union[dict, Request]
         The body of the request to be sent to the API. This must follow the structure described here:
         https://developers.google.com/qpx-express/v1/trips/search
     use_containers : Optional[bool]
@@ -84,6 +89,8 @@ def send_sync(request_body: dict, use_containers: bool=True):
     """
     if isinstance(request_body, dict):
         response = requester.post_request_sync(BASE_URL + api_key, request_body)
+    elif isinstance(request_body, Request):
+        response = requester.post_request_sync(BASE_URL + api_key, request_body.raw_data)
     else:
         raise ValueError('Unsupported Request Type')
     if use_containers:
